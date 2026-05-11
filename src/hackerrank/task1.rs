f47f69be77a6bb89ed069f6b6c9739290a1ab210
konovalovmyhaylo <konovalov.myhaylo@e-u.edu.ua>
2026-05-12 01:57:37 +0300

    Implement Task 1 solution for Practical Work 1

--- src/hackerrank/task1.rs
+++ src/hackerrank/task1.rs
@@ -1,3 +1,13 @@
-pub fn solve() {
-    println!("Hello from HackerRank task!");
+pub fn solve_me_first(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(solve_me_first(10, 5), 15);
+    }
 }
